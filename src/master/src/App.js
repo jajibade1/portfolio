601d0cbed2efac595db699983ceba1b97003bde9
Jamie <Jamie.Ajibade@relexsolutions.co.uk>
2019-02-23 01:29:03 +0000

    App

---
+++ src/master/src/App.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Jamie's Portfolio">
+        <header className="App-header">
+        <h1>
+          Welcome To Jamie's Portfolio
+        </h1>
+
+          <p>
+          Welcome to my portfolio website. Here I will demonstrate my web development background and programming knowledge via small projects and tasks I have completed. I would like to improve on my web development skills by creating websites and improve my knowledge of programming in Java, Node, Angular JS, HTML, CSS, C# and C++ as well as other technologies.
+          I have a BSc degree in information technology as a practitioner. I managed to achieve a first class honours degree at Coventry University. I have always enjoyed using computers and learning to program. I have managed to complete various projects within university and outside of my studies I have designed programs in C++ and C#. I have also completed a sample shopping website using HTML, CSS. 
+          </p>
+     
+
+      <h1>Projects</h1>
+
+      <h4>App Inventor tutorial</h4>
+
+      <p> 
+       This was a application I designed for a phone which was a quiz. It enabled the user to select the correct answer and it enabled the user to change the colour, text size and included text to speech function. In addtion, I was able to create different level varying from easy to hard. It would caluclate the amount of questions you got right and wrong.
+      </p>
+
+      <embed src="https://www.youtube.com/embed/nCDdHbUkRLk" />
+
+      <h4>Web shopping website</h4>
+
+      <p> 
+      This is a website I designed in HTML and CSS. It was a shopping website which enabled the users to select products from the images and it would calculate the total. It enabled the user to remove or add products to the basket.
+      </p>
+
+      <img
+      src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/a33d7450726913.58d847da0cc52.jpg"
+      />
+
+
+        <h1>
+        About Me
+        </h1>
+
+        <img
+      src="https://media.licdn.com/dms/image/C5603AQHcqBwe7hGknw/profile-displayphoto-shrink_200_200/0?e=1556150400&v=beta&t=4iEhIZhNz2G3N9bM3IXooDN4SoJ_zXeWYGmKeBasPVA"
+      />
+      
+        <p>
+          I enjoy listening to music, travelling, playing football and going to the gym. I also support Arsenal FC and enjoy watching basketball.
+      </p>
+
+        </header>
+        
+
+      </div>
+    );
+  }
+}
+
+export default App;
