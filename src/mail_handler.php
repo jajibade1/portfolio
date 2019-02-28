1471c8383be45d7a22ab6394e6da4357addadd4a
Jamie <Jamie.Ajibade@relexsolutions.co.uk>
2019-02-28 22:40:34 +0000

    email_handler.php added

--- src/mail_handler.php
+++ src/mail_handler.php
@@ -1,21 +1,25 @@
 <?php
-if(isset($_POST['Submit']))
+if(isset($_POST['Submit'])){
+
 $name = $_POST['Name'];
 $email = $_POST['Email'];
-$subject = $_POST['subject'];
+$subject = $_POST['subject'];  
 
 
-$to = 'jajibade1@gmail.com';
+$to='jajibade1@gmail.com';
 $subject='Form Submission';
-$message="Name: ".$name. "\n". "Email: ".$email. "\n". "\n\n".$subject;
+$message="Name: ".$name."\n". "Email: ".$email. "\n". "Subject: "."\n\n".$subject;
 $headers="From: ".$email;
-
 if(mail($to, $subject, $message, $headers)){
-    echo "<h1>Sent Successfully! Thamk you". " ".$name. "We will contact you shortly </h1>";
+    echo "<h1> Sent Successfully! Thank you .$name. We will contact you shortly! </h1>";
 
 }
 else{
     echo "Something went wrong!"
-    }
 }
+}
+
+
+
+
 ?>
