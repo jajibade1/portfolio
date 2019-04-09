9a5f204eed8cb18e78a7e69e0513629ead4c39d5
Jamie <Jamie.Ajibade@relexsolutions.co.uk>
2019-04-09 17:20:38 +0100

    website updated university studies

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
       <li><a href="#">Home</a></li>
       <li><a href="#project">Projects</a></li>
       <li><a href="#aboutme">About Me</a></li>
+      <li><a href="#portfolio">Portfolio</a></li>
       </ul>
       </header>
 
@@ -98,6 +99,212 @@ This is a website I designed in HTML and CSS. It was a shopping website which en
       <li id="aboutme">Contact Me</li>
       </ul>
       </div>
+
+      <div className="Projects">
+    <ul class="Project-Title">
+      <li id="portfolio">University Studies</li>
+      </ul>
+  
+
+    <h3>Course; Bsc (Hons) Information Technology Practitioner</h3>
+
+    <h3>Graduated: 2016</h3>
+
+    <b>Course Overview</b>
+
+    <p class="Review">This course is strongly focused on providing a critical understanding of Information Technology as an enabler for organisations and the complex and fast-changing trends of IT in a business context. It covers key processes, the role of information systems to support business functions, enterprise planning systems and their functional capabilities, and the role of the underpinning technologies. Management skills, organisational models, finance and accounting, intellectual property, the Internet and human resources management are also covered.</p>
+    
+    <h2>Modules Taught;</h2>
+
+    <div className="Projects">
+    <ul class="Project-Title">
+      <li id="portfolio">1st Year</li>
+      </ul>
+      </div>
+
+    <b>Information systems</b>
+    <ul>
+<li class="eview">Be able to demonstrate the use of various information systems in our society and how they are implemented to benefit businesses.
+</li>
+
+<br></br>
+
+<li>Methods and techniques used in businesses in order to plan, implement and maintain an information system.</li>
+
+<br></br>
+
+<li>How it benefits and saves businesses money and resources.</li>
+
+<br></br>
+
+Project: Designed a IT library system using Access, ERD, report queries.
+    </ul>
+
+    <b>IT in Business </b>
+    <ul>
+<li> How IT has aided businesses in day to day work.
+</li>
+
+<br></br>
+
+<li>How technology and IT is used to enhance a company and influence them in order to gain a competitive position in the market and over their competitors.</li>
+
+<br></br>
+
+<li>IT project management and standard project management intergrated in order to become successful.</li>
+
+<br></br>
+
+Project: Proposal and presentation on how IT can be used to enhance a company in a chosen sector.
+    </ul>
+
+    <b>Managing Networks </b>
+    <ul>
+<li> Understanding basic IT networking terminology and devices in order to create a network. </li>
+
+<br></br>
+
+<li> Mathematics and computer science in order to Map a network and build a cost table.</li>
+
+<br></br>
+
+Project: Report outlining thr key terms, structure and network for a particular organisation.
+
+<br></br>
+    </ul>
+
+    <b>Programming </b>
+    <ul>
+<li> Develop key understandings of programming concepts and methodologies used.</li>
+
+<br></br>
+
+<li> Full Stack development including planning, testing and maintaining the program.</li>
+
+<br></br>
+
+<li> Using appropriate pprogramming method and project management in order to design a software based on clients needs.</li>
+
+<br></br>
+Project: Designed a quote system for customers based on the building material chosen. This used input, output and process in order to calculate the quote. Programmed in C#.
+</ul>
+
+<div className="Projects">
+    <ul class="Project-Title">
+      <li id="portfolio">2nd Year</li>
+      </ul>
+      </div>
+<b>Designing Databases </b>
+    <ul>
+<li> Using various databases to design and develop a IT system based on a specification.</li>
+
+<br></br>
+
+<li> Understand how to use PHP, MYSQL, ERD and Entity relationships, Access.</li>
+
+<br></br>
+
+Project: Design an IT library booking system based to add, delete and search queries in the system. Use of reports and report findings in order to determine late fees and any other queries. Integration of HTML,PHP, MYSQL and Access.
+</ul>
+
+<b>Developing and Testing Systems</b>
+    <ul>
+<li>Design a IT system based on user requirements. </li>
+
+<br></br>
+
+<li> Develop and test the system</li>
+
+<br></br>
+
+<li> Design a user guide, how to guide and FAQ documentation. </li>
+
+<br></br>
+
+<li> Methodologies and techniques used for  designing the system. </li>
+
+<br></br>
+
+Project: Design a moodle system using WordPress, PHPMYADMIN. HTML, CSS, Plugins and Widgets.
+
+</ul>
+
+<b> Software Development </b>
+    <ul>
+<li> Design a mobile application using App inventor. </li>
+
+<br></br>
+
+<li> QR Scan to phone to install app </li>
+
+<br></br>
+
+<li> Plan, test and maintain application </li>
+
+
+<p>Project = <a href="//youtu.be/nCDdHbUkRLk">Quiz App</a></p>
+</ul>
+
+<div className="Projects">
+    <ul class="Project-Title">
+      <li id="portfolio">3rd Year</li>
+      </ul>
+      </div>
+
+<b> Implementing E-Commerce Solutions </b>
+    <ul>
+<li>Using E-Commerce such as WordPress in order to design a website based on any chosen sector. </li>
+
+<br></br>
+
+<li> Integrate payment methods, plugins, Widgets, HTML in order to design the system. </li>
+
+<br></br>
+
+<li> Understand key IT and business strategies in order to develop an E-Commerce business. </li>
+
+<br></br>
+
+Project: Designed a Restaurant website using WordPress.
+</ul>
+
+<b>Advanced Networking - Server Development and Maintenance  </b>
+    <ul>
+<li> Develop advanced networking knowledge and server skills.</li>
+
+<br></br>
+
+<li> Develop, test and maintain a server using Raspberry PI and Windows servers. </li>
+
+<br></br>
+
+<li> Use of HTML, PHP, IIS, installation and configuration of Windows and PI server features. </li>
+
+<br></br>
+
+<li>VPS, Active Directory, remote desktop service, virtualisation aand other networking technologies and features.</li>
+<br></br>
+
+Project: install, configure and maintain a server from stratch and using web technologies such as HTML, PHP,MYSQL to link the servers together.
+</ul>
+
+<div className="Projects">
+    <ul class="Project-Title">
+      <li id="portfolio">Certifications</li>
+      </ul>
+      </div>
+
+<br></br>
+
+<li> MTA NETWORKING FUNDAMENTALS </li>
+
+<br></br>
+
+<li>MTA WINDOWS SERVER FUNDAMENTAL  </li>
+
+<br></br>
+<br></br>
+    </div>
      
       <iframe class="form" src="https://docs.google.com/forms/d/e/1FAIpQLSdQPsBtWba2Gu5ccyQzfWLpiugFSd71x8r7Nizg5OgYJfCcEg/viewform?embedded=true" width="auto" height="1000" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>      
         
